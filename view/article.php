591587221de32e71a97bb529aae2cdef823147c6
jbaptiste <jb.queralt@gmail.com>
2018-07-05 19:17:55 +0200

    Commit Style .Update article.php
    .Upload CV_JBQ.pdf

--- view/article.php
+++ view/article.php
@@ -3,8 +3,7 @@ session_start();
 require('../controller/controller.php');
 
 
-if (!isset($_GET['id']) || $_GET['id'] <= 0)
-{
+if (!isset($_GET['id']) || $_GET['id'] <= 0) {
     echo "Cet identifiant d'article n'existe pas.";
     exit;
 }
