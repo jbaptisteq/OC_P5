27794ac79e5b7164c28a507238f739c9c5a2b1be
jbaptiste <jb.queralt@gmail.com>
2018-07-05 22:24:31 +0200

    feat adding Legal Notice Legal Notice add in footer.php

--- view/footer.php
+++ view/footer.php
@@ -30,38 +30,115 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        Copyright &copy; Jean-Baptiste Queralt 2018 - <a href="connexion.php" class="admin-link">Administration</a>
+                        Copyright &copy; Jean-Baptiste Queralt 2018 - <a href="" data-toggle="modal" data-target="#myModal">Mentions Légales</a> - <a href="connexion.php" class="admin-link">Administration</a>
                     </div>
                 </div>
             </div>
         </div>
     </footer>
 
-    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
-        <a class="btn btn-primary" href="#page-top">
-            <i class="fa fa-chevron-up"></i>
-        </a>
-    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Mentions Légales</h4>
+                </div>
+                <div class="modal-body">
+                    <p style="text-align:center">MENTIONS LEGALES :</p>
+                    <p style="text-align:justify">Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : <a href="http://www.localhost" target="_blank">www.localhost</a> les informations suivantes :</p>
+                    <p style="text-align:justify"><strong>1. Informations légales :</strong></p>
+                    <p style="text-align:justify">Statut du propriétaire : <strong>particulier</strong><br />
+                        Le Propriétaire est : <strong>jean-baptiste queralt</strong><br />
+                        Adresse postale du propriétaire : <strong>---</strong><br />
+                        <br />
+                        Le Créateur du site est : <strong>jean baptiste queralt</strong><br />
+                        Le Responsable de la  publication est : <strong>jean baptiste queralt</strong><br />
+                        Contacter le responsable de la publication : <strong>queralt.jb@orange.fr</strong><br />
+                        Le responsable de la publication est une<strong> personne physique</strong><br />
+                        <br />
+                        Le Webmaster est  : <strong>jean baptiste queralt</strong><br />
+                        Contacter le Webmaster :  <strong><a href="mailto:queralt.jb@orange.fr?subject=Contact a partir des mentions lÃ©gales via le site www.localhost">queralt.jb@orange.fr</a></strong><br />
+                        L’hebergeur du site est : <strong>localhost localhost --- </strong><br />
+                        <strong><u>CREDIT :</u> </strong> Les mentions légales ont étés générées par<strong> <a href="https://www.generer-mentions-legales.com/generateur-mentions-legales.html" target="_blank">générateur de mentions legales</a></strong><br />
+                        Renseignements sur la <a href="https://editioneo.com/formalites/declaration-cnil" target="_blank">conformité RGPD</a></p>
 
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+                        <p style="text-align:justify"> </p>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+                        <p style="text-align:justify"><strong>2. Présentation et principe :</strong></p>
 
-    <!-- Plugin JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+                        <p style="text-align:justify">Est désigné ci-après : <strong>Utilisateur</strong>, tout internaute se connectant et utilisant le site susnommé : <a href="http://www.localhost" target="_blank">www.localhost</a>.<br />
+                            Le site <strong>www.localhost</strong><strong> </strong>regroupe un ensemble de services, dans l'état,  mis à la disposition des utilisateurs. Il est ici précisé que ces derniers doivent rester courtois et faire preuve de bonne foi tant envers les autres utilisateurs qu'envers le webmaster du site www.localhost. Le site www.localhost est mis à jour régulièrement par jean baptiste queralt.<br />
+                            jean-baptiste queralt s’efforce de fournir sur le site www.localhost des informations les plus précises possibles (sous réserve de modifications apportées depuis leur mise en ligne), mais ne saurait garantir l'exactitude, la complétude et l'actualité des informations diffusées sur son site, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. En conséquence, l'utilisateur reconnaît utiliser ces informations données (à titre indicatif, non exhaustives et susceptibles d'évoluer) sous sa responsabilité exclusive.</p>
 
-    <!-- tinymce integration -->
-    <script>
-    $(document).ready(function() {
-        tinymce.init({
-            selector: '#articleContent'
+                        <p style="text-align:justify"> </p>
+
+                        <p style="text-align:justify"><strong>3. Accessibilité :</strong><br />
+                            <br />
+                            Le site www.localhost est par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au service, www.localhost s’engage à faire son maximum afin de rétablir l’accès au service et s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.  N’étant soumis qu’à une obligation de moyen, www.localhost ne saurait être tenu pour responsable de tout dommage, quelle qu’en soit la nature, résultant d’une indisponibilité du service.</p>
+
+                        <p style="text-align:justify"> </p>
+
+                        <p style="text-align:justify"><strong>4. Propriété intellectuelle :</strong></p>
+
+                        <p style="text-align:justify"><br />
+                            jean-baptiste queralt est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, tant sur la structure que sur les textes, images, graphismes, logo, icônes, sons, logiciels…<br />
+                            Toute reproduction totale ou partielle du site www.localhost, représentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de jean-baptiste queralt, propriétaire du site à l'email : queralt.jb@orange.fr, à défaut elle sera considérée comme constitutive d’une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+
+                        <p style="text-align:justify"><br />
+                            <strong>5. Liens hypertextes et cookies :</strong><br />
+                            <br />
+                            Le site www.localhost contient un certain nombre de liens hypertextes vers d’autres sites (partenaires, informations …) mis en place avec l’autorisation de jean-baptiste queralt. Cependant, jean-baptiste queralt n’a pas la possibilité de vérifier l'ensemble du contenu des sites ainsi visités et décline donc toute responsabilité de ce fait quand aux risques éventuels de contenus illicites.<br />
+                            L’utilisateur est informé que lors de ses visites sur le site www.localhost, un ou des cookies sont susceptibles de s’installer automatiquement sur son ordinateur par l'intermédiaire de son logiciel de navigation. Un cookie est un bloc de données qui ne permet pas d'identifier l'utilisateur, mais qui enregistre des informations relatives à la navigation de celui-ci sur le site. <br />
+                            Le paramétrage du logiciel de navigation permet d’informer de la présence de cookie et éventuellement, de la refuser de la manière décrite à l’adresse suivante : <a href="http://www.cnil.fr">www.cnil.fr</a>. L’utilisateur peut toutefois configurer le navigateur de son ordinateur pour refuser l’installation des cookies, sachant que le refus d'installation d'un cookie peut entraîner l’impossibilité d’accéder à certains services. Pour tout bloquage des cookies, tapez dans votre moteur de recherche : bloquage des cookies sous IE ou firefox et suivez les instructions en fonction de votre version.</p>
+
+                        <p style="text-align:justify"><br />
+                            <strong>6. Protection des biens et des personnes - gestion des données personnelles :</strong><br />
+                            <br />
+                            En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.</p>
+
+                        <p style="text-align:justify">Sur le site www.localhost, jean-baptiste queralt ne collecte des informations personnelles ( suivant l'article 4 loi n°78-17 du 06 janvier 1978) relatives à l'utilisateur que pour le besoin de certains services proposés par le site www.localhost. L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie. Il est alors précisé à l'utilisateur du site www.localhost l’obligation ou non de fournir ces informations.<br />
+                            Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification, de suppression et d’opposition aux données personnelles le concernant. Pour l’exercer, adressez votre demande à www.localhost par email : <strong><a href="mailto:queralt.jb@orange.fr?subject=Contact ï¿½ partir des mentions lï¿½gales via le site www.localhost">queralt.jb@orange.fr</a></strong> ou par écrit dûment signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.</p>
+
+                        <p style="text-align:justify">Aucune information personnelle de l'utilisateur du site www.localhost n'est publiée à l'insu de l'utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l'hypothèse du rachat du site www.localhost et de ses droits autorise jean-baptiste queralt à transmettre les dites informations à l'éventuel acquéreur qui serait à son tour tenu à la même obligation de conservation et de modification des données vis à vis de l'utilisateur du site www.localhost.<br />
+                            Le site www.localhost est en conformité avec le RGPD voir notre politique RGPD  none.</p>
+
+                        <p style="text-align:justify">Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+        <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+            <a class="btn btn-primary" href="#page-top">
+                <i class="fa fa-chevron-up"></i>
+            </a>
+        </div>
+
+        <!-- jQuery -->
+        <script src="../vendor/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+        <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+
+        <!-- tinymce integration -->
+        <script>
+        $(document).ready(function() {
+            tinymce.init({
+                selector: '#articleContent'
+            });
         });
-    });
-    </script>
-</body>
+        </script>
+    </body>
 
-</html>
+    </html>
