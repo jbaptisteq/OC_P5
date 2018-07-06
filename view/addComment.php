ade5169cbd05af2332fe5a00bda28a256d680181
jbaptiste <jb.queralt@gmail.com>
2018-07-06 10:07:57 +0200

    fix : Change result of 'if' test with error message and link to article's page. addComment.php

--- view/addComment.php
+++ view/addComment.php
@@ -9,15 +9,18 @@ require('../controller/controller.php');
 if (isset($_POST['blackIce'])) {
     if (empty($_SESSION['blackIce'])) {
         echo "Test Variable BlackIce non existantes ou vide.";
-        exit;
+        echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
+        return;
     }
     if ($_SESSION['blackIce'] !== $_POST['blackIce']) {
         echo "Les variables blackIce sont différentes.";
-        exit;
+        echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
+        return;
     }
     if (empty($_POST['authorComment']) || empty($_POST['content'])) {
         echo "Vous n'avez pas remplis tout les champs.";
-        exit;
+        echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
+        return;
     }
     newComment($_GET['id'], $_POST['authorComment'], $_POST['content']);
     header('Location: article.php?id='.$_GET['id']);
