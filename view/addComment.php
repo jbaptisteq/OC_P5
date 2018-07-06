e393d09154802387a89d37dea98c6d8bb4c677de
jbaptiste <jb.queralt@gmail.com>
2018-07-06 10:34:49 +0200

    docs : Change error message addComment.php

--- view/addComment.php
+++ view/addComment.php
@@ -8,17 +8,17 @@ require('../controller/controller.php');
  */
 if (isset($_POST['blackIce'])) {
     if (empty($_SESSION['blackIce'])) {
-        echo "Test Variable BlackIce non existantes ou vide.";
+        echo "Une erreur s'est produite lors de l'envoi de votre commentaire.";
         echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
         return;
     }
     if ($_SESSION['blackIce'] !== $_POST['blackIce']) {
-        echo "Les variables blackIce sont différentes.";
+        echo "Une erreur s'est produite lors de l'envoi de votre commentaire.";
         echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
         return;
     }
     if (empty($_POST['authorComment']) || empty($_POST['content'])) {
-        echo "Vous n'avez pas remplis tout les champs.";
+        echo "Une erreur s'est produite lors de l'envoi de votre commentaire.";
         echo '<br /><a href="article.php?id='.$_GET['id'].'">Retour à l\'article</a>';
         return;
     }
